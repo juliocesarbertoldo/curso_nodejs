5c6d8e6c213c87ef99415843c6fd4f338154a036
JulioCesarBertoldo <112409550+JulioCesarBertoldo@users.noreply.github.com>
2023-02-14 11:57:45 -0300

    a

---
+++ 09_introducaoMongoose/aprendendo.js
@@ -0,0 +1,49 @@
+//CONEXÃO COM O BANCO MOGODB
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+mongoose.connect('mongodb://127.0.0.1/aprendendo', {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('Conectado com sucesso!')
+}).catch((erro) => {
+    console.log('Houve um erro ao se conectar ao mongodb ' + erro)
+})
+
+//MODEL - USUARIOS
+//DEFININDO O MODEL
+const UsuarioSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    sobrenome: {
+        type: String
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number,
+        require: true
+    },
+    pais: {
+        type: String
+    }
+})
+//FALANDO A COLLECTION
+mongoose.model('usuarios', UsuarioSchema)
+
+//PASSAR OS DADOS
+const novoUsuario = mongoose.model('usuarios')
+new novoUsuario({
+    nome: 'Julio Cesar',
+    sobrenome: 'Bertoldo',
+    idade: 32,
+    pais: 'Brasil'
+}).save().then(() => {
+    console.log('Usuario criado com sucesso!')
+}).catch((erro) => {
+    console.log('Houve um erro ao criar o usuario' + erro)
+})

b7449b235400def6c65470dd853f340808bb2f9a
JulioCesarBertoldo <112409550+JulioCesarBertoldo@users.noreply.github.com>
2023-02-22 18:06:12 -0300

    v

---
+++ 10_blogApp/routes/admin.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Categoria')
+const Categoria = mongoose.model('categorias')
+
+router.get('/', (req, res) => {
+    res.render('admin/index')
+})
+
+router.get('/post', (req, res) => {
+    res.send('Página de posts')
+})
+
+router.get('/categorias', (req, res) => {
+    res.render('admin/categorias')
+})
+
+router.get('/categorias/add', (req, res) => {
+    res.render('admin/addcategorias')
+})
+
+router.post('/categorias/nova', (req, res) => {
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(() => {
+        console.log('Categoria salva com sucesso')
+    }).catch((erro) => {
+        console.log('Erro ao salvar' + erro)
+    })
+})
+
+module.exports = router

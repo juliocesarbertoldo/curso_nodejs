6eeff7603b8ff49786a175c14cb227adb71f81ee
JulioCesarBertoldo <112409550+JulioCesarBertoldo@users.noreply.github.com>
2023-02-23 18:11:00 -0300

    .

--- 10_blogApp/routes/admin.js
+++ 10_blogApp/routes/admin.js
@@ -9,11 +9,17 @@ router.get('/', (req, res) => {
 })
 
 router.get('/post', (req, res) => {
-    res.send('Página de posts')
+    res.render('admin/posts')
 })
 
 router.get('/categorias', (req, res) => {
-    res.render('admin/categorias')
+    Categoria.find().sort({date: 'desc'}).then((categorias) => {
+        res.render('admin/categorias', {categorias: categorias})
+
+    }).catch((erro) => {
+        req.flash('error_msg', 'Houve um erro ao listar as categorias')
+        res.redirect('/admin')
+    })
 })
 
 router.get('/categorias/add', (req, res) => {
@@ -21,16 +27,44 @@ router.get('/categorias/add', (req, res) => {
 })
 
 router.post('/categorias/nova', (req, res) => {
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+    var erros = []
+
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+        erros.push({ texto: 'Nome inválido!' })
     }
 
-    new Categoria(novaCategoria).save().then(() => {
-        console.log('Categoria salva com sucesso')
-    }).catch((erro) => {
-        console.log('Erro ao salvar' + erro)
+    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        erros.push({ texto: 'Slug inválido!' })
+    }
+
+    if (req.body.nome.length <= 2) {
+        erros.push({ texto: 'Nome da categoria muito pequeno' })
+    }
+
+    if (erros.length > 0) {
+        res.render('admin/addcategorias', { erros: erros })
+    } else {
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash('success_msg', 'Categoria criada com sucesso!')
+            res.redirect('/admin/categorias')
+        }).catch((erro) => {
+            req.flash('error_msg', 'Houve um erro ao salvar a categoria, tente novamente!')
+            res.redirect('/admin')
+        })
+
+    }
+})
+
+router.get('/categorias/edit/:id', (req, res) => {
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+        res.render('admin/editcategoria', {categoria: categoria})
     })
+    res.render('admin/editcategoria')
 })
 
 module.exports = router
